c0269a9b6bda27fc6e485a003df3a5bbf64a6997
c1t1zen <kmkennedy@gmail.com>
2023-08-10 00:13:25 -0700

    update with images from Bimg
    Fark column working
    - After prompt
    - sanitized headline name

---
+++ HeadlineImageUpdater_FARK_05b4.mjs
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import { generateImageFiles } from "./dist/index.js";
+import fs from 'fs';
+import path from 'path';
+import cheerio from 'cheerio';
+
+function sanitizeHeadline(headline) {
+    // Replace special characters with underscores
+    const sanitized = headline.replace(/[^A-Za-z0-9_\-]/g, ' ').replace(/:/g, '');
+    return sanitized;
+}
+
+async function fetchFarkHeadline() {
+    try {
+        const response = await axios.get('http://138.2.232.184');
+        const content = response.data;
+
+        const $ = cheerio.load(content);
+        const headline = $('h2:contains("Fark")').next().find('li a').eq(4).text().trim();
+
+        if (headline) {
+            return headline;
+        } else {
+            console.error("Failed to extract the Fark 5 headline using cheerio.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching the Fark 5 headline:", error);
+        return null;
+    }
+}
+
+// ... [rest of the imports and functions]
+async function generateImagesFromHeadline() {
+    const headline = await fetchFarkHeadline();
+    if (headline) {
+      // console.log("Fetched Fark 5 Headline:", headline);
+      try {
+        const sanitizedHeadline = sanitizeHeadline(headline); // Sanitize the headline
+        const imagePath = `File:///C:/tmp/${sanitizedHeadline}/0.jpeg`;
+        console.log("Fark 5 Headline:", sanitizedHeadline);
+        
+        const directory = path.join(sanitizedHeadline); // Create the directory path
+        // console.log("Directory:", directory);
+
+        const imageFiles = await generateImageFiles(directory); // Pass the directory to the generateImageFiles function
+
+        console.log("Fark 5 Headline:", directory);
+
+        const indexContent = fs.readFileSync('C:/Users/WindowsX/Documents/Code/Bimg/index.html', 'utf-8');
+        const imgTag = `<center><br><img src="${imagePath}" alt="Generated Image" width="98%"></center>`; // Added <br> here
+
+        const $ = cheerio.load(indexContent);
+        const FarkHeadlineElement = $('h2:contains("Fark")').next().find('li a').eq(4);
+        FarkHeadlineElement.after(imgTag);
+
+        fs.writeFileSync('C:/Users/WindowsX/Documents/Code/Bimg/index.html', $.html());
+            console.log("index.html updated with generated image after the Fark 5 headline.");
+        } catch (error) {
+            console.error("Error generating images:", error);
+            
+            // Handle the error by generating a new image with the given prompt
+            try {
+                const fallbackImageFiles = await generateImageFiles("Parrot wearing a fedora and dressed like a reporter with a notepad");
+                const fallbackImageData = fallbackImageFiles[0].data;
+
+                let indexContent = fs.readFileSync('C:/Users/WindowsX/Documents/Code/Bimg/index.html', 'utf-8');
+                const $ = cheerio.load(indexContent);
+                const FarkHeadlineElement = $('h2:contains("Fark")').next().find('li a').eq(4);
+                const imgTag = `<center><br><img src="data:image/jpeg;base64,${fallbackImageData}" alt="Fallback Image" width="98%"></center>`;
+                FarkHeadlineElement.after(imgTag);
+
+                fs.writeFileSync('C:/Users/WindowsX/Documents/Code/Bimg/index.html', $.html());
+                console.log("index.html updated with fallback Parrot Pete image due to an error.");
+            } catch (fallbackError) {
+                console.error("Error generating fallback image:", fallbackError);
+            }
+        }
+    } else {
+        console.log("Failed to fetch the Fark 5 headline.");
+    }
+}
+
+generateImagesFromHeadline();
+
+
+
+
+
