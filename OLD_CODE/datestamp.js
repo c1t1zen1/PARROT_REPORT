7da82c0b40a241d217659230392e4b0b682b91fb
c1t1zen <kmkennedy@gmail.com>
2023-09-02 20:48:54 -0700

    update v 0.2.6
    Added Dark Mode
    Fixed Fonts

--- OLD_CODE/datestamp.js
+++ OLD_CODE/datestamp.js
@@ -10,8 +10,6 @@ const datestamp = currentDate.toLocaleString();
 const $ = cheerio.load(indexContent);
 
 // Append the datestamp to the body, which will place it just before the closing </body> tag
-$('body').append(`\n<p style="text-align: center;">PARROT REPORT: ${datestamp} UTC</p><p style="text-align: center;"><a href="https://c1t1zen.com/PR/archive.html" style="text-decoration: none; color: black;">Explore The Archives</a></p><br>`);
-
-
+$('body').append(`\n<p style="text-align: center;">PARROT REPORT: ${datestamp} UTC</p>`);
 
 fs.writeFileSync('./index2.html', $.html());
--- sanitize.js
+++ sanitize.js
@@ -1,12 +1,13 @@
 // sanitize keywords - masterlist
-
+// V0.2.6 - August 28 2023
 // Sanitize the headline by replacing sensitive words, celebrity names, custom terms, and apostrophes
 
 export function sanitizeHeadline(headline) {
-  const sensitiveWords = ['Trump', 'Republican', 'Democrat', 'Authorit', 'Conservative', 'Liberals'];
-  const celebrityMaleNames = ['Jeff Bezos', 'Mark Zuckerberg', 'Elon Musk', 'DeSantis'];
-  const celebrityFemaleNames = ['Oprah', 'Jennifer Lopez', 'Paltrow'];
-  const customReplacements = [{ original: 'MAGA', replacement: 'Maddness' }, { original: 'Assault', replacement: 'punch' }, { original: 'Thomas', replacement: 'Bigsby' }, { original: 'Supreme Court Justice', replacement: 'judge' }, { original: 'dead', replacement: 'cartoon zombie' }, { original: 'nsfw', replacement: 'safe' }, { original: 'Hunter Biden', replacement: 'middle aged scruffy skinny white guy' }, { original: 'Biden', replacement: 'really old tired skinny white guy' }, { original: 'nymphs', replacement: 'cartoon fairy' }, { original: 'illegally raid', replacement: 'kindly knock on' }, { original: 'repressive', replacement: 'overly friendly' }, { original: 'Barack Obama', replacement: 'tall skinny older black man' }, { original: 'Obama', replacement: 'old skinny graying hair black man in suit' }, { original: 'lazy', replacement: 'tired' }, { original: 'shrewd', replacement: 'patient' }, { original: 'brat', replacement: 'rich kid' }, { original: 'Epstein', replacement: 'tall skinny older man' }, { original: 'Holocaust', replacement: 'old timey war' }, { original: 'libel', replacement: 'words' }, { original: 'fake ID', replacement: 'bad card' }, { original: 'banned', replacement: 'kicked out' }, { original: 'Senator Feinstein', replacement: 'old italian white lady' }, { original: 'Elvis Presley', replacement: 'Early Rock n Roll start' }, { original: 'Rebelling', replacement: 'acting out' }, { original: 'shoots', replacement: 'shouts' }, { original: 'sued', replacement: 'given papers' }, { original: 'EVERYBODY PANIC', replacement: 'keep calm' }, { original: 'deadly', replacement: 'big' }, { original: 'problem', replacement: 'issue' }, { original: 'mouth off', replacement: 'loud' }, { original: 'sucking', replacement: 'being boring' }, { original: 'steals', replacement: 'borrows' }, { original: 'breaks', replacement: 'opens' }, { original: 'sets fire', replacement: 'blowtorch' }, { original: 'Holiday Inn Express', replacement: 'cheap motel' }, { original: 'Shooting', replacement: 'responsible cat owner' }, { original: 'Abuse', replacement: 'care' }, { original: 'Homelessness', replacement: 'living in tents' }, { original: 'Criminalized', replacement: 'looked at by police' }, { original: 'guilty', replacement: 'looking odd' }, { original: 'stupid', replacement: 'silly' }, { original: 'bomb', replacement: 'Acme TNT' }, { original: 'bestiality', replacement: 'animals playing' }, { original: 'Cock', replacement: 'Rooster' }, { original: 'boobies', replacement: 'melons' }, { original: 'poisoning', replacement: 'magic potion' }, { original: 'Plagiarism', replacement: 'Copying' }, { original: 'Hardcore', replacement: 'Skateboarding punk' }, { original: 'Pubic', replacement: 'underwear' }, { original: 'revenge-porn', replacement: 'home video' }];
+  const sensitiveWords = ['Trump', 'Republican', 'Democrat', 'Authorit', 'Conservative', 'Liberal', 'Rudy Giuliani', 'Bill Clinton', 'Epstein', 'Valdimir', 'Zelensky', 'Clinton', 'Roger Stone'];
+  const celebrityMaleNames = ['Jeff Bezos', 'Mark Zuckerberg', 'Elon Musk', 'DeSantis', 'Jimmy Carter', 'Putin', 'Elon', 'Musk'];
+  const celebrityFemaleNames = ['Oprah', 'Jennifer Lopez', 'Paltrow', 'Hilary', 'Hillary'];
+  const customReplacements = [{ original: 'MAGA', replacement: 'Maddness' }, { original: 'Assault', replacement: 'punch' }, { original: 'Thomas', replacement: 'Bigsby' }, { original: 'Supreme Court Justice', replacement: 'judge' }, { original: 'dead', replacement: 'skeleton' }, { original: 'nsfw', replacement: 'safe' }, { original: 'Hunter Biden', replacement: 'middle aged scruffy skinny white guy' }, { original: 'Biden', replacement: 'really old tired skinny white guy' }, { original: 'nymphs', replacement: 'cartoon fairy' }, { original: 'illegally raid', replacement: 'kindly knock on' }, { original: 'repressive', replacement: 'overly friendly' }, { original: 'Barack Obama', replacement: 'tall skinny older black man' }, { original: 'Obama', replacement: 'old skinny graying hair black man in suit' }, { original: 'lazy', replacement: 'tired' }, { original: 'shrewd', replacement: 'patient' }, { original: 'brat', replacement: 'rich kid' }, { original: 'Epstein', replacement: 'tall skinny older man' }, { original: 'Holocaust', replacement: 'old timey war' }, { original: 'libel', replacement: 'words' }, { original: 'fake ID', replacement: 'bad card' }, { original: 'banned', replacement: 'kicked out' }, { original: 'Senator Feinstein', replacement: 'old italian lady' }, { original: 'Elvis Presley', replacement: 'Early Rock n Roll start' }, { original: 'Rebelling', replacement: 'acting out' }, { original: 'shoots', replacement: 'shouts' }, { original: 'sued', replacement: 'given papers' }, { original: 'EVERYBODY PANIC', replacement: 'keep calm' }, { original: 'deadly', replacement: 'big' }, { original: 'problem', replacement: 'issue' }, { original: 'mouth off', replacement: 'loud' }, { original: 'sucking', replacement: 'being boring' }, { original: 'steals', replacement: 'borrows' }, { original: 'breaks', replacement: 'opens' }, { original: 'sets fire', replacement: 'blowtorch' }, { original: 'Holiday Inn Express', replacement: 'cheap motel' }, { original: 'Shooting', replacement: 'responsible cat owner' }, { original: 'Abuse', replacement: 'care' }, { original: 'Homelessness', replacement: 'living in tents' }, { original: 'Criminalized', replacement: 'looked at by police' }, { original: 'guilty', replacement: 'looking odd' }, { original: 'stupid', replacement: 'silly' }, { original: 'bomb', replacement: 'Acme TNT' }, { original: 'bestiality', replacement: 'animals playing' }];
+  const customReplacements2 = [{ original: 'Cock', replacement: 'Rooster' }, { original: 'boobies', replacement: 'melons' }, { original: 'poisoning', replacement: 'magic potion' }, { original: 'Plagiarism', replacement: 'Copying' }, { original: 'Hardcore', replacement: 'muscles' }, { original: 'Pubic', replacement: 'underwear' }, { original: 'revenge-porn', replacement: 'home video' }, { original: 'penis', replacement: 'foot' }, { original: 'rogue', replacement: 'on its own' }, { original: 'love life', replacement: 'dating scene' }, { original: 'Slave', replacement: 'human' }, { original: 'Hot Cousin', replacement: 'Blonde' }, { original: 'Hooligans', replacement: 'rough human' }, { original: 'Narcissist', replacement: 'Big Ego' }, { original: 'Crappy', replacement: 'not great' }, { original: 'Fuck', replacement: 'Hella' }, { original: 'Pope Francis', replacement: 'octagenarian with tall white religious hat' }, { original: 'Masturbating', replacement: 'hands together' }, { original: 'Orgy', replacement: 'humans wrestling' }, { original: 'wreck', replacement: 'distorted' }, { original: 'genocide', replacement: 'battle' }];
 
   // Replace political sensitive words with clown
   for (const word of sensitiveWords) {
@@ -32,6 +33,12 @@ export function sanitizeHeadline(headline) {
     headline = headline.replace(regex, replacement);
   }
 
+  // Replace custom terms 2
+   for (const { original, replacement } of customReplacements2) {
+    const regex = new RegExp(`\\b${original}\\b`, 'gi');
+    headline = headline.replace(regex, replacement);
+  }
+
   // Replace apostrophes without inserting a space
   headline = headline.replace(/'/g, '');
 
@@ -39,8 +46,11 @@ export function sanitizeHeadline(headline) {
   const sanitized = headline.replace(/[^\w\s]/g, '').replace(/\s+/g, ' ');
 
   // Limit the sanitized headline to a specific length
-  const maxLength = 225; // Example: Limit to 255 characters
-  const truncated = sanitized.slice(0, maxLength);
+  const maxLength = 255; // Max 255 characters
+  let truncated = sanitized.slice(0, maxLength);
+
+  // Trim any trailing spaces
+  truncated = truncated.trim();
 
   return truncated;
 }
