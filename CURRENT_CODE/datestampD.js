d7e6809938eaca153074e10b254e608935e3ce64
c1t1zen <kmkennedy@gmail.com>
2023-09-24 19:24:43 -0700

    Fix Datestamp
    Fixed:
    - centered div datestamp

--- CURRENT_CODE/datestampD.js
+++ CURRENT_CODE/datestampD.js
@@ -6,12 +6,17 @@ import cheerio from 'cheerio';
 const indexContent = fs.readFileSync('./indexD.html', 'utf-8');
 const currentDate = new Date();
 const datestamp = currentDate.toLocaleString();
-
 const $ = cheerio.load(indexContent);
 
-// Append the datestamp to the body, which will place it just before the closing </body> tag
-$('body').append(`\n<p style="text-align: center;">PARROT REPORT: ${datestamp} UTC</p><p style="text-align: center;"><a href="https://c1t1zen.com/PR/archiveD.html" style="text-decoration: none; color: inherit;">Explore The Archives</a></p><br>`);
+// Create a container div that spans the width of the page
+const containerDiv = `
+<div style="width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+    <p style="text-align: center;">PARROT REPORT: ${datestamp} UTC</p>
+    <p style="text-align: center;"><a href="https://c1t1zen.com/PR/archiveD.html" style="text-decoration: none; color: inherit;">Explore The Archives</a></p>
+</div>
+<br>
+`;
 
-
-
+// Append the container div to the body, which will place it just before the closing </body> tag
+$('body').append(containerDiv);
 fs.writeFileSync('./indexD.html', $.html());
